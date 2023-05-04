dec4f03769eb34a325e2575c25b6ec023468966d
qqKazakov <leap74rus@gmail.com>
2023-05-04 08:03:41 +0300

    fix: admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
-from .models import Tag
-from .models import Task
 
-admin.site.register(User, UserAdmin, Tag, Task)
+
+admin.site.register(User, UserAdmin)
