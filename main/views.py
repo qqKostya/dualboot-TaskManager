32380bc158672ba60d1c8d3c8e6bc4a36b5cb84a
qqKazakov <leap74rus@gmail.com>
2023-05-12 06:33:58 +0300

    add: views filter user

--- main/views.py
+++ main/views.py
@@ -1,11 +1,20 @@
 from rest_framework import viewsets
+import django_filters
 from .models import User, Tag, Task
 from .serializers import UserSerializer, TagSerializer, TaskSerializer
 
 
+class UserFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr="icontains")
+
+    class Meta:
+        model = User
+        fields = ('name',)
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
+    filterset_class = UserFilter
 
 
 class TagViewSet(viewsets.ModelViewSet):
