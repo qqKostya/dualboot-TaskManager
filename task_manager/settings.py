a9a9eba28df0d08e03fb07cab59efb6fcf5dc7b5
qqKazakov <leap74rus@gmail.com>
2023-05-16 06:11:55 +0300

    add: IsAuthenticated

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -135,4 +135,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    'DEFAULT_PERMISSION_CLASSES': ("rest_framework.permissions.IsAuthenticated",),
 }
