b089b5fc79d03cd46339fe5995552ca5c35d22dd
qqKazakov <leap74rus@gmail.com>
2023-07-11 06:40:29 +0300

    add: new two mixin

--- task_manager/services/single_resource.py
+++ task_manager/services/single_resource.py
@@ -1,7 +1,16 @@
 import copy
 from typing import Any, List
-
+from requests import Request
+from rest_framework.response import Response
 from rest_framework import routers
+from typing import Any, List, TYPE_CHECKING
+
+from rest_framework import viewsets
+
+if TYPE_CHECKING:
+    BaseViewMixinBaseClass = viewsets.GenericViewSet
+else:
+    BaseViewMixinBaseClass = object
 
 
 class BulkRouter(routers.SimpleRouter):
@@ -11,3 +20,28 @@ class BulkRouter(routers.SimpleRouter):
         self.routes: List[routers.Route] = copy.deepcopy(self.routes)
         self.routes[0].mapping.update({"patch": "partial_bulk_update"})
         self.routes[0].mapping.update({"put": "bulk_update"})
+
+
+class SingleResourceMixin(BaseViewMixinBaseClass):
+    pagination_class = None
+
+    def list(self, _: Request, *__: Any, **___: Any) -> Response:
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+    
+
+class SingleResourceUpdateMixin(BaseViewMixinBaseClass):
+    def bulk_update(self, request: Request, *_: Any, **kwargs: Any) -> Response:
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_bulk_update(
+        self, request: Request, *args: Any, **kwargs: Any
+    ) -> Response:
+        kwargs["partial"] = True
+        return self.bulk_update(request, *args, **kwargs)
