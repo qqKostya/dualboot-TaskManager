4ea30cd52f22589cd97bc18f7d56875653690d94
qqKazakov <leap74rus@gmail.com>
2023-07-11 06:28:08 +0300

    add: single_resource

---
+++ task_manager/services/single_resource.py
@@ -0,0 +1,13 @@
+import copy
+from typing import Any, List
+
+from rest_framework import routers
+
+
+class BulkRouter(routers.SimpleRouter):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.routes: List[routers.Route] = copy.deepcopy(self.routes)
+        self.routes[0].mapping.update({"patch": "partial_bulk_update"})
+        self.routes[0].mapping.update({"put": "bulk_update"})
